cf1ccd24f72d704c7b2251583f164ed2bed50c10
JJ Zabkar <jjzabkar@gmail.com>
2019-03-18 11:19:55 -0600

    initial commit

---
+++ src/main/java/com/tangocard/hackathon/streaming/streamingwhatevs/StreamingwhatevsApplication.java
@@ -0,0 +1,13 @@
+package com.tangocard.hackathon.streaming.streamingwhatevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingwhatevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingwhatevsApplication.class, args);
+	}
+
+}
